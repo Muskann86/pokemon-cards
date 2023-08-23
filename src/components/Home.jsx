149c526a2a724c18210d3a3e5fff3c4293497a11
Fatoumata SOUKOUNA <soukounafatoumata86@gmail.com>
2023-08-23 22:05:49 +0200

    change the width of the card

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,31 +21,31 @@ function Home() {
 
   return (
     <div className="d-grid justify-content-center container">
-      {isLogged ? <div className="card my-3 rounded border-dark" style={{ width: "18rem"}}>
-          <img
-            src={pokemon?.sprites.other["official-artwork"].front_default}
-            className="card-img-top"
-            alt="Pokémon"
-          />
-          <div className="card-body">
-            <h5 className="card-title fw-bold fs-3 text-capitalize">
-              Name : {pokemon?.name}
-            </h5>
-            <ul className="list-unstyled">
-              <li className="card-text fw-bold fs-3">
-                Height : {pokemon?.height}
+      {isLogged ? <div className="card my-3 rounded border-dark" style={{ width: "24rem" }}>
+        <img
+          src={pokemon?.sprites.other["official-artwork"].front_default}
+          className="card-img-top"
+          alt="Pokémon"
+        />
+        <div className="card-body">
+          <h5 className="card-title fw-bold fs-3 text-capitalize">
+            Name : {pokemon?.name}
+          </h5>
+          <ul className="list-unstyled">
+            <li className="card-text fw-bold fs-3">
+              Height : {pokemon?.height}
+            </li>
+            <li className="card-text fw-bold fs-3">
+              Weight : {pokemon?.weight}
+            </li>
+          </ul>
+          <ul className="list-group list-group-flush">
+            {pokemon?.types.map((type) => (
+              <li className="card-footer list-group-item text-capitalize fw-bold fs-2">
+                Type : {type.type.name}
               </li>
-              <li className="card-text fw-bold fs-3">
-                Weight : {pokemon?.weight}
-              </li>
-            </ul>
-            <ul className="list-group list-group-flush">
-              {pokemon?.types.map((type) => (
-                <li className="card-footer list-group-item text-capitalize fw-bold fs-2">
-                  Type : {type.type.name}
-                </li>
-              ))}
-            </ul>
+            ))}
+          </ul>
           <button
             onClick={() => setId(randomNumber)}
             className="btn btn-lg d-grid gap-2 mx-auto fw-bold fs-5 text-white text-uppercase"
@@ -53,10 +53,10 @@ function Home() {
           >
             Get a pokemon
           </button>
-          </div>
         </div>
-       : 
-       <Link to="/login" className="fw-bold fs-2 text-uppercase">You need to be Logged in</Link> 
+      </div>
+        :
+        <Link to="/login" className="fw-bold fs-2 text-uppercase">You need to be Logged in</Link>
       }
     </div>
   );
